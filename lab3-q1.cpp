ffd2e2c5621fd01fafc0e1fa43ef083ca9d0cdde
KAMALESHKAR <42136274+KAMALESHKAR@users.noreply.github.com>
2018-08-17 11:40:43 +0530

    Add files via upload

---
+++ lab3-q1.cpp
@@ -0,0 +1,20 @@
+//first include the library
+#include<iostream>
+using namespace std;
+//write function
+int main(){
+//declaration of data types
+int p;
+float q;
+char r;
+bool d;
+double e;
+//getting sizes
+cout <<"size of int is"<<sizeof (p) <<endl;
+cout <<"size of float is"<<sizeof (q) <<endl;
+cout <<"size of char is"<<sizeof (r) <<endl;
+cout <<"size of bool is"<<sizeof (d) <<endl;
+cout <<"size of double is"<<sizeof (e) <<endl;
+//terminate
+return 0;}
+
