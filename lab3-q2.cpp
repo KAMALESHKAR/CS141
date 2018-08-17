d6a1af106946f399b5aa61a0c0dc3622839778c1
KAMALESHKAR <42136274+KAMALESHKAR@users.noreply.github.com>
2018-08-17 11:41:07 +0530

    Add files via upload

---
+++ lab3-q2.cpp
@@ -0,0 +1,25 @@
+//incude the library
+#include<iostream>
+using namespace std;
+//write the function
+int main(){// declaring variables
+int a, b;
+//process
+//give values to variables
+a = 1500;
+b = 900;
+//get result
+//by adding
+cout <<"sum is";cout <<a + b<<endl;
+//by subtracting b from a
+cout <<"difference is";cout <<a - b<<endl;
+//by multiplying
+cout <<"product is";cout <<a * b<<endl;
+//by dividing to get quotent
+cout <<"quotient is";cout <<a / b<<endl;
+// by operating modulus to get remainder
+cout <<"remainder is";cout<<a % b;
+//terminate
+return 2;}
+
+
