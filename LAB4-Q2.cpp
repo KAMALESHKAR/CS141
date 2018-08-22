b83ce04b2c86206ca9c07d4f4866e39a222d50cc
KAMALESHKAR <42136274+KAMALESHKAR@users.noreply.github.com>
2018-08-22 14:39:04 +0530

    Add files via upload

---
+++ LAB4-Q2.cpp
@@ -0,0 +1,14 @@
+//include the library
+#include<iostream>
+using namespace std;
+//write the function
+int main(){
+cout <<"what is the temperature in celsius"<<endl;
+//declare variable
+double L;
+//assign value to it via terminal which is temp. in celsius
+cin >>L;
+//multiply it with 1.8 and add 32 to get temp. in farenheit
+cout <<"temperature in farenheit is "<<L*1.8+32<<endl;
+//TERMINATE
+return 0;}
