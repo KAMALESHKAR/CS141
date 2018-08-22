5be0657f0ff988121bd6d0db6705e42edea573e9
KAMALESHKAR <42136274+KAMALESHKAR@users.noreply.github.com>
2018-08-22 16:24:04 +0530

    Add files via upload

---
+++ LAB4-Q5.cpp
@@ -0,0 +1,21 @@
+//include the library
+#include<iostream>
+using namespace std;
+//write the function
+int main(){
+//ask for 1st angle
+cout <<"what is the 1ST angle in degrees"<<endl;
+//declare 1st variable
+float D;
+//assign value via terminal
+cin >>D;
+//ask for 2nd angle
+cout <<"what is the 2ND angle in dergrees"<<endl;
+//declare 2nd variable
+float E;
+//assign value through terminal
+cin >>E;
+//subtracting both of them from 180 to get the third angle in degrees
+cout <<"the 3RD angle is " <<180-D-E <<endl;
+//TERMINATE
+return 0;}
