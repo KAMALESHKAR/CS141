7a8025f30d6ba154c3ac08a6f2d5c8524a9108e4
KAMALESHKAR <42136274+KAMALESHKAR@users.noreply.github.com>
2018-09-12 22:48:51 +0530

    Add files via upload

---
+++ LAB5.5 SQUARE STAR.cpp
@@ -0,0 +1,17 @@
+//include the library
+#include<iostream>
+#include<cmath>
+using namespace std;
+//write the function
+int main(){
+//DECLARE NO. OF ROWS AND COLUMNS AND ASSIGN VALUE THROUGH TERMINAL
+int R, C;//R STANDS FOR ROWS C FOR COLUMNS
+cout <<"give no. of rows and columns ";
+cin >>R >>C;
+//WRITE LOOP TO MAKE NO. OF ROWS
+for(int i = 1;i <= R;i++){
+//WRITE LOOP TO MAKE NO. OF COLUMNS
+for(int j = 1;j <= C;j++){
+cout <<"*";}cout <<endl;}
+//TERMINATE
+return 0;}
