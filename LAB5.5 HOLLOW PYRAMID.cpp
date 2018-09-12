04a237de59d5bd35c4dc87510806eacb46d822dc
KAMALESHKAR <42136274+KAMALESHKAR@users.noreply.github.com>
2018-09-12 23:07:06 +0530

    Add files via upload

--- LAB5.5 HOLLOW PYRAMID.cpp
+++ LAB5.5 HOLLOW PYRAMID.cpp
@@ -4,15 +4,21 @@ int main()
 {
 //ASK FOR HEIGHT OF TRIANGE NEEDED
 cout <<"give height of triangle needed ";
+//GET HEIGHT
 int H;
 cin >>H;
+//LOOP FOR ROWS
 	for(int i=1;i<=H;i++)
+//LOOP FOR COLUMNS
 	{for(int j=1;j<=(H*2)-1;j++)
-		{if((j==(H-i+1))||(j==(H+i-1))||(i==H))
-					{cout <<"*";}
+		{
+//* AT PLACES NEEDED
+			if((j==(H-i+1))||(j==(H+i-1))||(i==H)){cout <<"*";}
+//OTHERWISE GIVE SPACE
 		else{cout <<" ";}
 		}
 cout <<endl;	
 	}
+//TERMINATE
 return 0;
 }
