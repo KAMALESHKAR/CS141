24b9ddc1d67804b2cce99ec88939a26369e6d859
KAMALESHKAR <42136274+KAMALESHKAR@users.noreply.github.com>
2018-09-12 23:21:32 +0530

    Add files via upload

--- LAB5 Q3 CONDITON.cpp
+++ LAB5 Q3 CONDITON.cpp
@@ -7,6 +7,7 @@ int main(){
 //DECLARE TWO VARIABLES AND ASSIGN VALUE THROUGH TERMINAL
 double a;
 cin >> a;
+//CHECK IF INPUT IS +VE,-VE OR 0 AND SHOW THE SAME
 if (a > 0){cout <<a <<" is positive";}
 else if (a == 0){cout <<"it is zero";}
 else {cout <<a <<" is negative" <<endl;}
