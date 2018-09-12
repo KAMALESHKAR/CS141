d2c657f9f295f6dc2a632034c4bc8219d6078919
KAMALESHKAR <42136274+KAMALESHKAR@users.noreply.github.com>
2018-09-12 23:35:58 +0530

    Add files via upload

---
+++ LAB5 Q5 CONDITION.cpp
@@ -0,0 +1,14 @@
+//include the library
+#include<iostream>
+#include<cmath>
+using namespace std;
+//write the function
+int main(){
+//DECLARE TWO VARIABLES AND ASSIGN VALUE THROUGH TERMINAL
+int a;
+cin >> a;
+//CHECK IF INPUT IS ODD OR EVEN AND SHOW THE SAME
+	if ((a%2==0)){cout <<a <<" is even";}
+	else {cout <<a <<" is odd" <<endl;}
+//TERMINATE
+return 0;}
